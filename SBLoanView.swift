2b8c7f7c77a9f3799dbb86fe2470160650d2fdee
gilmar Junior <gilmar_mendonca@live.com>
2025-05-19 22:27:28 -0300

    first commit

---
+++ SBLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  SBLoanView.swift
+//  Pods
+//
+//  Created by Gilmar Manoel de Mendonça Junior on 18/05/25.
+//
+
+import SwiftUI
+
+struct SBLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
